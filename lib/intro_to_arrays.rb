54cd58dbf047e653757967082641aeb56e81654e
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 17:56:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,23 @@ end
 def array_with_two_elements
   my_two_array = ["Dragunity", "Blue-Eyes"]
 end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
